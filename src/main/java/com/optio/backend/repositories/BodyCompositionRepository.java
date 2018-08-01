1def44b1340162694ff24b66e3ed21211ae0ce45
Rishni <Rishni@DESKTOP-7681NTT>
2018-08-01 08:29:27 +0530

    body comp modification

---
+++ src/main/java/com/optio/backend/repositories/BodyCompositionRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.optio.backend.repositories;
+
+import com.optio.backend.models.BodyComposition;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Rishni
+ */
+public interface BodyCompositionRepository extends CrudRepository<BodyComposition, String>{
+
+    @Override
+	void delete(BodyComposition deleted);
+    
+}
