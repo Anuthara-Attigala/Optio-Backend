b1507ab0414c37a2f770efbbe149a02b5eadeb89
Rishni <Rishni@DESKTOP-7681NTT>
2018-11-11 11:36:20 +0530

    fitness

--- src/main/java/com/optio/backend/BackendApplication.java
+++ src/main/java/com/optio/backend/BackendApplication.java
@@ -13,52 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-            //test();
-            //keywordSearch();
-		SpringApplication.run(BackendApplication.class, args);
-		//comment
-	}
+	public static void main(String[] args) {            
+		SpringApplication.run(BackendApplication.class, args);		
+	}      
         
-        /**public static void test(){
-            List<String> keywords = new ArrayList(Arrays.asList("FIRSTNAME", "LASTNAME", "FUND"));
-            List<String> dataList = new ArrayList(Arrays.asList("HUSBANDFIRSTNAME", "HUSBANDLASTNAME", "WIFEFIRSTNAME", "SOURCECURRENCY", "CURRENCYRATE"));
-            Set<String> targetSet = new HashSet();
-
-            String pattern = String.join("|", keywords);
-            System.out.println(pattern);
-            for (String data : dataList) {
-                Matcher matcher = Pattern.compile(pattern).matcher(data);
-                if (matcher.find()) {
-                    
-                    targetSet.add(data);
-                }
-            }
-
-            System.out.println(targetSet);
-        }
-        
-        public static void keywordSearch(){
-            String input = "first name";
-            input = input.replaceAll(" ", "|");
-            
-            //System.out.println(input);
-            
-            //System.out.println("*************************************************");
-            List<String> keywords = new ArrayList(Arrays.asList("FIRST NAME", "LAST NAME", "FUND"));
-            List<String> dataList = new ArrayList(Arrays.asList("HUSBAND FIRST NAME", "HUSBAND LAST NAME", "WIFEFIRSTNAME", "SOURCECURRENCY", "CURRENCYRATE"));
-            Set<String> targetSet = new HashSet();
-
-            String pattern = input;
-          //  System.out.println(pattern);
-            for (String data : dataList) {
-                Matcher matcher = Pattern.compile(pattern).matcher(data.toLowerCase());
-                if (matcher.find()) {
-                    
-                    targetSet.add(data);
-                }
-            }
-
-            System.out.println(targetSet);
-        }**/
 }
